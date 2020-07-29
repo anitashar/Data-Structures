aeabc9a4b4047ee178f22546eb70713fe0a80892
anitashar <anitashar2006@gmail.com>
2020-07-28 20:31:02 -0700

    finished binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -9,28 +9,70 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
+
 class BSTNode:
     def __init__(self, value):
+        # current node's value
         self.value = value
+
         self.left = None
         self.right = None
 
-    # Insert the given value into the tree
+        # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # check whether new node's value is less than current node's value
+        if value < self.value:
+            if not self.left: # if there is no node on the left the n create a new node
+                self.left = BSTNode(value)
+            else: # if there is a node on the left start the function over 
+                self.left.insert(value)
+        # check whether new node's value is greater than or equal to curr node's val
+        elif value >= self.value:
+            if not self.right: # if there is no node on the right the n create a new node
+                self.right = BSTNode(value)
+            else: # if there is a node on the right start the function over
+                self.right.insert(value)
+
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
-
+        # check whether curr node matches target
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:  #if there is no node left 
+                return False
+            else:   # if there node on the left then repeat the function
+                return self.left.contains(target)
+        else:
+            if not self.right: #if there is no node on the right
+                return False
+            else: # if there node on the right then repeat the function
+                return self.right.contains(target)
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # self.left.value will always be smaller than the root
+        # so we have to check only the self.right path
+        if not self.right:
+            return self.value
+        else:
+            return self.right.get_max()
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # call the function
+        fn(self.value)
+        # go to the left node if any
+        if self.left:
+            self.left.for_each(fn)
+        # go to the right node if any
+        if self.right:
+            self.right.for_each(fn)
+
+        
 
     # Part 2 -----------------------
 
@@ -48,38 +90,35 @@ class BSTNode:
     # in an iterative depth first traversal
     def dft_print(self):
         pass
-
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
     def pre_order_dft(self):
         pass
-
     # Print Post-order recursive DFT
     def post_order_dft(self):
         pass
-
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+# bst = BSTNode(1)
 
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+# bst.bft_print()
+# bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()  
